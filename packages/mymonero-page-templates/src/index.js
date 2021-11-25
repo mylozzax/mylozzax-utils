f506224785375f716f2f63587fb2b6fca1d6be57
Karl Buys <kbbuys@gmail.com>
2021-11-25 10:56:01 +0200

    feat: import sendfunds view in CJS index

--- packages/mymonero-page-templates/src/index.js
+++ packages/mymonero-page-templates/src/index.js
@@ -7,13 +7,18 @@
 require("./Exchange/Elements/ChangenowBuyWithFiatView");
 require("./Exchange/Elements/ChangenowFixedRateView")
 require("./Exchange/Elements/ChangenowFloatingRateView");
-
+require("./SendFunds/Elements/SendFundsView");
 /* Import exchange landing page class */
 let ExchangeLandingPage = require("./Exchange/Elements/ExchangeLandingPage");
 let ExchangeNavigationController = require("./Exchange/Controllers/ExchangeNavigationController");
 
+let SendFundsView = require("./Exchange/Controllers/SendFundsView");
+let SendFundsNavigationController = require("./Exchange/Controllers/SendFundsNavigationController");
+
 /* Export page templates */
 module.exports = {
     ExchangeLandingPage,
-    ExchangeNavigationController
+    ExchangeNavigationController,
+    SendFundsView,
+    SendFundsNavigationController
 }

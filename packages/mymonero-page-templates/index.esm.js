6099e14e726a98cf29a4c018407eeeab249e030d
Karl Buys <kbbuys@gmail.com>
2021-11-26 14:54:43 +0200

    feat(authentication): add AuthenticationView to index files

--- packages/mymonero-page-templates/index.esm.js
+++ packages/mymonero-page-templates/index.esm.js
@@ -6,6 +6,9 @@ require("./src/Exchange/Elements/ChangenowFixedRateView")
 require("./src/Exchange/Elements/ChangenowFloatingRateView");
 require("./src/SendFunds/Elements/SendFundsView");
 
+import AuthenticationLandingPage from "./src/Authentication/Elements/AuthenticationView");
+import AuthenticationController from "./src/Authentication/Controller/AuthenticationController");
+
  /* Import exchange landing page class */
 import ExchangeLandingPage from "./src/Exchange/Elements/ExchangeLandingPage";
 import ExchangeNavigationController from "./src/Exchange/Controllers/ExchangeNavigationController";
@@ -14,6 +17,8 @@ import SendFundsController from "./src/SendFunds/Controllers/SendFundsController
 
  /* Export page templates */
  export default {
+    AuthenticationView,
+    AuthenticationLandingPage,
     ExchangeLandingPage,
     ExchangeNavigationController,
     SendFundsView,
--- packages/mymonero-page-templates/src/index.js
+++ packages/mymonero-page-templates/src/index.js
@@ -4,11 +4,15 @@
 //require("@mymonero/mymonero-web-components");
 
 /* Require various view elements */
+
 require("./Exchange/Elements/ChangenowBuyWithFiatView");
 require("./Exchange/Elements/ChangenowFixedRateView")
 require("./Exchange/Elements/ChangenowFloatingRateView");
 require("./SendFunds/Elements/SendFundsView");
 /* Import exchange landing page class */
+let AuthenticationLandingPage = require("./Authentication/Elements/AuthenticationView");
+let AuthenticationController = require("./Authentication/Controller/AuthenticationController");
+
 let ExchangeLandingPage = require("./Exchange/Elements/ExchangeLandingPage");
 let ExchangeNavigationController = require("./Exchange/Controllers/ExchangeNavigationController");
 
@@ -17,8 +21,9 @@ let SendFundsNavigationController = require("./Exchange/Controllers/SendFundsNav
 
 /* Export page templates */
 module.exports = {
+    AuthenticationView,
     ExchangeLandingPage,
     ExchangeNavigationController,
     SendFundsView,
-    SendFundsNavigationController
+    SendFundsController
 }

23bcc1c3b6b90836798223b7d7cf0bda84edabd3
Karl Buys <kbbuys@gmail.com>
2021-11-26 12:47:08 +0200

    feat(message-dialog): add message dialog import to index

--- packages/mymonero-web-components/index.esm.js
+++ packages/mymonero-web-components/index.esm.js
@@ -1,10 +1,13 @@
 /* Require reusable components */
 require("./src/Components/Shared/ActivityIndicator");
 require("./src/Components/Shared/ContactPicker");
+require("./src/Components/Shared/MessageDialog");
 require("./src/Components/Shared/ProviderCard");
 require("./src/Components/Shared/SearchableSelect")
 require("./src/Components/Shared/Tooltip");
 require("./src/Components/Shared/WalletSelector");
 
-/* Export page templates */
+/*  Export page templates 
+ *  We technically don't need any exports since this module just registers custom elements
+ * */
 export default { }
